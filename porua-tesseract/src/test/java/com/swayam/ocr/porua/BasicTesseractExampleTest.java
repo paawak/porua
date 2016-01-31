e686b0bd17769cef1a609c9de3ffb1aa119c6eb1
paawak@gmail.com <paawak@gmail.com>
2016-01-31 18:56:50 -0500

    added asserts to test text returned by tesseract

--- porua-tesseract/src/test/java/com/swayam/ocr/porua/BasicTesseractExampleTest.java
+++ porua-tesseract/src/test/java/com/swayam/ocr/porua/BasicTesseractExampleTest.java
@@ -2,7 +2,13 @@ package com.swayam.ocr.porua;
 
 import static org.bytedeco.javacpp.lept.pixDestroy;
 import static org.bytedeco.javacpp.lept.pixRead;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 import org.bytedeco.javacpp.BytePointer;
 import org.bytedeco.javacpp.lept.PIX;
@@ -21,26 +27,29 @@ public class BasicTesseractExampleTest {
 		PIX image = pixRead(imageFile);
 		BytePointer outText = null;
 
-		try (TessBaseAPI api = new TessBaseAPI();) {
-			if (api.Init("/usr/share/tesseract/", "eng") != 0) {
-				System.err.println("Could not initialize tesseract.");
-				System.exit(1);
-			}
-
-			api.SetImage(image);
-			// Get OCR result
-			outText = api.GetUTF8Text();
-			String string = outText.getString();
-			assertTrue(!string.isEmpty());
-			System.out.println("OCR output:\n" + string);
-
-			// Destroy used object and release memory
-			api.End();
-
-		} finally {
-			outText.deallocate();
-			pixDestroy(image);
+		TessBaseAPI api = new TessBaseAPI();
+		if (api.Init("/usr/share/tesseract/", "eng") != 0) {
+			fail("Could not initialize tesseract.");
 		}
+
+		api.SetImage(image);
+		// Get OCR result
+		outText = api.GetUTF8Text();
+		String ocrText = outText.getString().trim();
+
+		// Destroy used object and release memory
+		api.End();
+		api.close();
+		outText.deallocate();
+		pixDestroy(image);
+
+		System.out.printf("OCR output:%s\n", ocrText);
+		assertTrue(!ocrText.isEmpty());
+
+		String expected = readFile(BasicTesseractExampleTest.class.getResource(
+				"/com/swayam/ocr/porua/res/test-english.txt").getPath());
+
+		assertEquals(expected, ocrText);
 
 	}
 
@@ -54,27 +63,47 @@ public class BasicTesseractExampleTest {
 		PIX image = pixRead(imageFile);
 		BytePointer outText = null;
 
-		try (TessBaseAPI api = new TessBaseAPI();) {
-			if (api.Init("/usr/share/tesseract/", "ben") != 0) {
-				System.err.println("Could not initialize tesseract.");
-				System.exit(1);
-			}
-
-			api.SetImage(image);
-			// Get OCR result
-			outText = api.GetUTF8Text();
-			String string = outText.getString();
-			assertTrue(!string.isEmpty());
-			System.out.println("OCR output:\n" + string);
-
-			// Destroy used object and release memory
-			api.End();
-
-		} finally {
-			outText.deallocate();
-			pixDestroy(image);
+		TessBaseAPI api = new TessBaseAPI();
+		if (api.Init("/usr/share/tesseract/", "ben") != 0) {
+			fail("Could not initialize tesseract.");
 		}
+
+		api.SetImage(image);
+		// Get OCR result
+		outText = api.GetUTF8Text();
+		String ocrText = outText.getString().trim();
+
+		// Destroy used object and release memory
+		api.End();
+		api.close();
+		outText.deallocate();
+		pixDestroy(image);
+
+		System.out.printf("OCR output:%s\n", ocrText);
+		assertTrue(!ocrText.isEmpty());
+
+		String expected = readFile(BasicTesseractExampleTest.class.getResource(
+				"/com/swayam/ocr/porua/res/Bangla-300-short.txt").getPath());
+
+		assertEquals(expected, ocrText);
 
 	}
 
+	private String readFile(String path) {
+
+		StringBuilder allLines = new StringBuilder(200);
+
+		try (BufferedReader reader = new BufferedReader(new FileReader(path));) {
+
+			reader.lines().forEach((String line) -> {
+				allLines.append(line).append("\n");
+			});
+
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		return allLines.toString().trim();
+
+	}
 }
