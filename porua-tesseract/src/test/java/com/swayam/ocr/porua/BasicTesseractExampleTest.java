5e392a405ac0cf0e0944dc53e100730aa5b3170b
Palash Ray <paawak@gmail.com>
2016-03-14 00:17:07 -0400

    get bounding box from tesseract api

--- porua-tesseract/src/test/java/com/swayam/ocr/porua/BasicTesseractExampleTest.java
+++ porua-tesseract/src/test/java/com/swayam/ocr/porua/BasicTesseractExampleTest.java
@@ -1,5 +1,7 @@
 package com.swayam.ocr.porua;
 
+import static org.bytedeco.javacpp.lept.L_CLONE;
+import static org.bytedeco.javacpp.lept.boxaGetBox;
 import static org.bytedeco.javacpp.lept.pixDestroy;
 import static org.bytedeco.javacpp.lept.pixRead;
 import static org.junit.Assert.assertEquals;
@@ -9,9 +11,14 @@ import static org.junit.Assert.fail;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.IntBuffer;
 
 import org.bytedeco.javacpp.BytePointer;
+import org.bytedeco.javacpp.lept.BOX;
+import org.bytedeco.javacpp.lept.BOXA;
 import org.bytedeco.javacpp.lept.PIX;
+import org.bytedeco.javacpp.lept.PIXA;
+import org.bytedeco.javacpp.tesseract;
 import org.bytedeco.javacpp.tesseract.TessBaseAPI;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -82,6 +89,38 @@ public class BasicTesseractExampleTest {
 		String expected = readFile(BasicTesseractExampleTest.class.getResource("/com/swayam/ocr/porua/res/Bangla-300-short.txt").getPath());
 
 		assertEquals(expected, ocrText);
+	}
+
+	@Test
+	public void givenTessBaseApi_getBoundingBox() throws Exception {
+		String imageFile = "/home/paawak/kaaj/code/porua/porua-frontend/image-store/training/bangla/rajshekhar-basu-mahabharat/Bangla-mahabharat-1-page_1/Bangla-mahabharat-1-page_1.png";
+		PIX image = pixRead(imageFile);
+
+		TessBaseAPI api = new TessBaseAPI();
+		if (api.Init(TESSDATA, "ben") != 0) {
+			fail("Could not initialize tesseract.");
+		}
+
+		api.SetImage(image);
+		BOXA boxes = api.GetComponentImages(tesseract.RIL_TEXTLINE, true, (PIXA) null, (IntBuffer) null);
+
+		System.out.printf("Found %d textline image components.%n", boxes.n());
+
+		for (int i = 0; i < boxes.n(); i++) {
+			BOX box = boxaGetBox(boxes, i, L_CLONE);
+			api.SetRectangle(box.x(), box.y(), box.w(), box.h());
+			BytePointer ocrResult = api.GetUTF8Text();
+			String ocrText = ocrResult.getString().trim();
+			ocrResult.deallocate();
+			int conf = api.MeanTextConf();
+			System.out.printf("Box[%d]: x=%d, y=%d, w=%d, h=%d, confidence: %d, text: %s%n", i, box.x(), box.y(), box.w(), box.h(), conf, ocrText);
+		}
+
+		// Destroy used object and release memory
+		api.End();
+		api.close();
+		pixDestroy(image);
+
 	}
 
 	@Ignore
