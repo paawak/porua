202498d932259ea0f18be885f8eceabd7b3399ab
Palash Ray <paawak@gmail.com>
2016-03-14 00:41:25 -0400

    Result iterator example

--- porua-tesseract/src/test/java/com/swayam/ocr/porua/BasicTesseractExampleTest.java
+++ porua-tesseract/src/test/java/com/swayam/ocr/porua/BasicTesseractExampleTest.java
@@ -19,6 +19,7 @@ import org.bytedeco.javacpp.lept.BOXA;
 import org.bytedeco.javacpp.lept.PIX;
 import org.bytedeco.javacpp.lept.PIXA;
 import org.bytedeco.javacpp.tesseract;
+import org.bytedeco.javacpp.tesseract.ResultIterator;
 import org.bytedeco.javacpp.tesseract.TessBaseAPI;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -92,7 +93,7 @@ public class BasicTesseractExampleTest {
 	}
 
 	@Test
-	public void givenTessBaseApi_getBoundingBox() throws Exception {
+	public void givenTessBaseApi_GetComponentImages() throws Exception {
 		String imageFile = "/home/paawak/kaaj/code/porua/porua-frontend/image-store/training/bangla/rajshekhar-basu-mahabharat/Bangla-mahabharat-1-page_1/Bangla-mahabharat-1-page_1.png";
 		PIX image = pixRead(imageFile);
 
@@ -114,6 +115,42 @@ public class BasicTesseractExampleTest {
 			ocrResult.deallocate();
 			int conf = api.MeanTextConf();
 			System.out.printf("Box[%d]: x=%d, y=%d, w=%d, h=%d, confidence: %d, text: %s%n", i, box.x(), box.y(), box.w(), box.h(), conf, ocrText);
+		}
+
+		// Destroy used object and release memory
+		api.End();
+		api.close();
+		pixDestroy(image);
+
+	}
+
+	@Test
+	public void givenTessBaseApi_ResultIterator() throws Exception {
+		String imageFile = "/home/paawak/kaaj/code/porua/porua-frontend/image-store/training/bangla/rajshekhar-basu-mahabharat/Bangla-mahabharat-1-page_1/Bangla-mahabharat-1-page_1.png";
+		PIX image = pixRead(imageFile);
+
+		TessBaseAPI api = new TessBaseAPI();
+		if (api.Init(TESSDATA, "ben") != 0) {
+			fail("Could not initialize tesseract.");
+		}
+
+		api.SetImage(image);
+		api.Recognize(null);
+
+		ResultIterator ri = api.GetIterator();
+		int level = tesseract.RIL_WORD;
+
+		while (ri.Next(level)) {
+			BytePointer word = ri.GetUTF8Text(level);
+			String ocrText = word.getString().trim();
+			float conf = ri.Confidence(level);
+			IntBuffer x1 = IntBuffer.allocate(50);
+			IntBuffer y1 = IntBuffer.allocate(50);
+			IntBuffer x2 = IntBuffer.allocate(50);
+			IntBuffer y2 = IntBuffer.allocate(50);
+
+			ri.BoundingBox(level, x1, y1, x2, y2);
+			System.out.printf("word: '%s';  \tconf: %.2f; BoundingBox: %d,%d,%d,%d;%n", ocrText, conf, x1.get(), y1.get(), x2.get(), y2.get());
 		}
 
 		// Destroy used object and release memory
