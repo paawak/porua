18f70f69f3c79aeb2d83a117fc067d9b0f756555
Palash Ray <paawak@gmail.com>
2016-11-06 20:03:10 +0530

    fool proof

--- porua-frontend/src/test/java/com/swayam/ocr/test/HttpClientTest.java
+++ porua-frontend/src/test/java/com/swayam/ocr/test/HttpClientTest.java
@@ -18,15 +18,23 @@ package com.swayam.ocr.test;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.CharBuffer;
 import java.util.concurrent.CountDownLatch;
 
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpException;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.concurrent.FutureCallback;
 import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
 import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.apache.http.nio.IOControl;
+import org.apache.http.nio.client.methods.AsyncCharConsumer;
+import org.apache.http.nio.client.methods.HttpAsyncMethods;
+import org.apache.http.nio.protocol.HttpAsyncRequestProducer;
+import org.apache.http.protocol.HttpContext;
 import org.junit.Test;
 
 /**
@@ -35,8 +43,10 @@ import org.junit.Test;
  */
 public class HttpClientTest {
 
+    private static final String BANGLA_URL = "http://www.rabindra-rachanabali.nltr.org/node/1";
+
     @Test
-    public void test() throws InterruptedException {
+    public void test_1() throws InterruptedException {
 
         CountDownLatch countDownLatch = new CountDownLatch(1);
 
@@ -45,8 +55,7 @@ public class HttpClientTest {
         httpclient.start();
 
         // Execute request
-        httpclient.execute(
-                new HttpGet("http://www.rabindra-rachanabali.nltr.org/node/1"),
+        httpclient.execute(new HttpGet(BANGLA_URL),
                 new FutureCallback<HttpResponse>() {
 
                     @Override
@@ -98,9 +107,85 @@ public class HttpClientTest {
 
     }
 
+    @Test
+    public void test_2() throws InterruptedException {
+
+        CloseableHttpAsyncClient httpclient = HttpAsyncClients.createDefault();
+        // Start the client
+        httpclient.start();
+
+        final CountDownLatch latch2 = new CountDownLatch(1);
+        final HttpGet request3 = new HttpGet(BANGLA_URL);
+        HttpAsyncRequestProducer producer3 = HttpAsyncMethods.create(request3);
+        AsyncCharConsumer<HttpResponse> consumer3 = new AsyncCharConsumer<HttpResponse>() {
+
+            @Override
+            protected void onCharReceived(CharBuffer buf, IOControl ioctrl)
+                    throws IOException {
+
+                StringBuilder sb = new StringBuilder();
+
+                while (buf.hasRemaining()) {
+                    sb.append(buf.get());
+                }
+
+                System.out.println("**** " + sb.toString());
+
+            }
+
+            @Override
+            protected void onResponseReceived(HttpResponse response)
+                    throws HttpException, IOException {
+
+                System.out.println("111111111111111111");
+
+                if (response.getStatusLine().getStatusCode() == 200) {
+                    Header header = response.getFirstHeader("content-type");
+                    System.out.println("content-type: " + header.getValue());
+                }
+            }
+
+            @Override
+            protected HttpResponse buildResult(HttpContext context)
+                    throws Exception {
+                // TODO Auto-generated method stub
+                return null;
+            }
+
+        };
+        httpclient.execute(producer3, consumer3,
+                new FutureCallback<HttpResponse>() {
+
+                    @Override
+                    public void completed(final HttpResponse response3) {
+
+                        System.out.println("***********************");
+
+                        latch2.countDown();
+                    }
+
+                    @Override
+                    public void failed(final Exception ex) {
+                        System.out
+                                .println(request3.getRequestLine() + "->" + ex);
+
+                        latch2.countDown();
+                    }
+
+                    @Override
+                    public void cancelled() {
+                        System.out.println(
+                                request3.getRequestLine() + " cancelled");
+                        latch2.countDown();
+                    }
+
+                });
+        latch2.await();
+    }
+
     private String read(InputStream instream) throws IOException {
 
-        InputStreamReader reader = new InputStreamReader(instream, "utf8");
+        Reader reader = new InputStreamReader(instream, "utf8");
 
         StringBuilder sb = new StringBuilder(10_000);
 
