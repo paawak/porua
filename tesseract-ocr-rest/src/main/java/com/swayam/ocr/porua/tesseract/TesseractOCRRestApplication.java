878c9ca0ad0b5a00b51602007184de98a295bfc7
Palash Ray <paawak@gmail.com>
2018-07-01 23:11:52 +0530

    main class

---
+++ tesseract-ocr-rest/src/main/java/com/swayam/ocr/porua/tesseract/TesseractOCRRestApplication.java
@@ -0,0 +1,12 @@
+package com.swayam.ocr.porua.tesseract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesseractOCRRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesseractOCRRestApplication.class, args);
+	}
+}
