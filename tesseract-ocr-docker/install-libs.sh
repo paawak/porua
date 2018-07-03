5ffa38185e9db3852bcaa7d43c79a4e54c372d0c
Palash Ray <paawak@gmail.com>
2018-07-03 16:37:33 +0530

    bash script example

---
+++ tesseract-ocr-docker/install-libs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+libSrc="lib-sources/"
+libDest="libs/"
+for file in $(ls $libSrc)
+do
+	echo "File: $file"
+	dir=$(echo $file | sed -e 's,.tar.gz$,,g;s,.tar.xz$,,g')
+	echo "Unpacking in: $dir ..."
+	mkdir -p $libDest$dir
+	tar -xvf $libSrc$file -C $libDest$dir
+done
+
