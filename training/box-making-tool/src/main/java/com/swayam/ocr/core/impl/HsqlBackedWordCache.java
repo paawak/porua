b6fb9b611f468cf25ffcb1cae0ad9637014cd1ec
Palash Ray <paawak@gmail.com>
2020-04-26 23:04:58 +0530

    impl

--- training/box-making-tool/src/main/java/com/swayam/ocr/core/impl/HsqlBackedWordCache.java
+++ training/box-making-tool/src/main/java/com/swayam/ocr/core/impl/HsqlBackedWordCache.java
@@ -6,9 +6,12 @@ import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.function.Function;
 
 import org.slf4j.Logger;
@@ -29,14 +32,30 @@ public class HsqlBackedWordCache implements WordCache {
 
     @Override
     public void clearAllEntries() {
-	// TODO Auto-generated method stub
+	String sql = "TRUNCATE TABLE ocr_word";
 
+	executePreparedStatement(pstat -> {
+	    try {
+		pstat.executeUpdate();
+	    } catch (SQLException e) {
+		LOG.warn("failed to truncate table ocr_word", e);
+	    }
+	    return null;
+	}, sql);
     }
 
     @Override
     public int getWordCount() {
-	// TODO Auto-generated method stub
-	return 0;
+	String sql = "SELECT COUNT(*) FROM ocr_word";
+
+	return executePreparedStatement(pstat -> {
+	    try (ResultSet res = pstat.executeQuery()) {
+		res.next();
+		return res.getInt(1);
+	    } catch (SQLException e) {
+		throw new RuntimeException(e);
+	    }
+	}, sql);
     }
 
     @Override
@@ -58,26 +77,69 @@ public class HsqlBackedWordCache implements WordCache {
 
     @Override
     public Collection<CachedOcrText> getWords() {
-	// TODO Auto-generated method stub
-	return null;
+	String sql = "SELECT id, raw_ocr_word, corrected_text FROM ocr_word";
+	return executePreparedStatement(pstat -> {
+	    try (ResultSet res = pstat.executeQuery()) {
+		List<CachedOcrText> words = new ArrayList<>();
+		while (res.next()) {
+		    words.add(getCachedOcrText(res));
+		}
+		return words;
+	    } catch (SQLException e) {
+		throw new RuntimeException(e);
+	    }
+	}, sql);
     }
 
     @Override
     public CachedOcrText getWord(int wordId) {
-	// TODO Auto-generated method stub
-	return null;
+	String sql = "SELECT id, raw_ocr_word, corrected_text FROM ocr_word WHERE id = ?";
+	return executePreparedStatement(pstat -> {
+	    try {
+		pstat.setInt(1, wordId);
+	    } catch (SQLException e) {
+		throw new RuntimeException(e);
+	    }
+	    try (ResultSet res = pstat.executeQuery()) {
+		if (res.next()) {
+		    return getCachedOcrText(res);
+		}
+		return null;
+	    } catch (SQLException e) {
+		throw new RuntimeException(e);
+	    }
+	}, sql);
     }
 
     @Override
     public void modifyWord(int wordId, String text) {
-	// TODO Auto-generated method stub
+	String sql = "UPDATE ocr_word SET corrected_text = ? WHERE id = ?";
 
+	executePreparedStatement(pstat -> {
+	    try {
+		pstat.setString(1, text);
+		pstat.setInt(2, wordId);
+		pstat.executeUpdate();
+	    } catch (SQLException e) {
+		throw new RuntimeException(e);
+	    }
+	    return null;
+	}, sql);
     }
 
     @Override
     public void removeWord(int wordId) {
-	// TODO Auto-generated method stub
+	String sql = "DELETE FROM ocr_word WHERE id = ?";
 
+	executePreparedStatement(pstat -> {
+	    try {
+		pstat.setInt(1, wordId);
+		pstat.executeUpdate();
+	    } catch (SQLException e) {
+		throw new RuntimeException(e);
+	    }
+	    return null;
+	}, sql);
     }
 
     private void initSchema() {
@@ -112,6 +174,13 @@ public class HsqlBackedWordCache implements WordCache {
 	    throw new RuntimeException(e);
 	}
 
+    }
+
+    private CachedOcrText getCachedOcrText(ResultSet res) throws SQLException {
+	int id = res.getInt(1);
+	RawOcrWord rawOcrWord = (RawOcrWord) res.getObject(2);
+	String correctedText = res.getString(3);
+	return new CachedOcrText(id, rawOcrWord, correctedText);
     }
 
     private <R> R executePreparedStatement(Function<PreparedStatement, R> task, String sql) {
--- training/box-making-tool/src/test/java/com/swayam/ocr/core/impl/HsqlBackedWordCacheIntegrationTest.java
+++ training/box-making-tool/src/test/java/com/swayam/ocr/core/impl/HsqlBackedWordCacheIntegrationTest.java
@@ -69,4 +69,128 @@ class HsqlBackedWordCacheIntegrationTest {
 	assertEquals(expected, results);
     }
 
+    @Test
+    void testRemoveWord() throws SQLException {
+	// given
+	RawOcrWord rawOcrWord1 = new RawOcrWord(11, 22, 33, 44, 55.55f, "ABC123");
+	RawOcrWord rawOcrWord2 = new RawOcrWord(111, 222, 333, 444, 555.555f, "DEF456");
+	RawOcrWord rawOcrWord3 = new RawOcrWord(1111, 2222, 3333, 4444, 5555.5555f, "GHI789");
+	Collection<RawOcrWord> rawTexts = Arrays.asList(rawOcrWord1, rawOcrWord2, rawOcrWord3);
+
+	List<CachedOcrText> expected = Arrays.asList(new CachedOcrText(1, rawOcrWord1, null), new CachedOcrText(3, rawOcrWord3, null));
+
+	HsqlBackedWordCache testClass = new HsqlBackedWordCache();
+	testClass.storeRawOcrWords(rawTexts);
+
+	// when
+	testClass.removeWord(2);
+
+	// then
+	String sql = "SELECT id, raw_ocr_word, corrected_text FROM ocr_word";
+	List<CachedOcrText> results = new ArrayList<>();
+	try (PreparedStatement pStat = con.prepareStatement(sql); ResultSet res = pStat.executeQuery();) {
+	    while (res.next()) {
+		int id = res.getInt(1);
+		RawOcrWord rawOcrWord = (RawOcrWord) res.getObject(2);
+		String correctedText = res.getString(3);
+		results.add(new CachedOcrText(id, rawOcrWord, correctedText));
+	    }
+	}
+
+	assertEquals(expected, results);
+    }
+
+    @Test
+    void testClearAllEntries() throws SQLException {
+	// given
+	RawOcrWord rawOcrWord1 = new RawOcrWord(11, 22, 33, 44, 55.55f, "ABC123");
+	RawOcrWord rawOcrWord2 = new RawOcrWord(111, 222, 333, 444, 555.555f, "DEF456");
+	RawOcrWord rawOcrWord3 = new RawOcrWord(1111, 2222, 3333, 4444, 5555.5555f, "GHI789");
+	Collection<RawOcrWord> rawTexts = Arrays.asList(rawOcrWord1, rawOcrWord2, rawOcrWord3);
+
+	HsqlBackedWordCache testClass = new HsqlBackedWordCache();
+	testClass.storeRawOcrWords(rawTexts);
+
+	// when
+	testClass.clearAllEntries();
+
+	// then
+	String sql = "SELECT COUNT(*) FROM ocr_word";
+	try (PreparedStatement pStat = con.prepareStatement(sql); ResultSet res = pStat.executeQuery();) {
+	    res.next();
+	    int count = res.getInt(1);
+	    assertEquals(0, count);
+	}
+
+    }
+
+    @Test
+    void testModifyWord() throws SQLException {
+	// given
+	RawOcrWord rawOcrWord1 = new RawOcrWord(11, 22, 33, 44, 55.55f, "ABC123");
+	RawOcrWord rawOcrWord2 = new RawOcrWord(111, 222, 333, 444, 555.555f, "DEF456");
+	RawOcrWord rawOcrWord3 = new RawOcrWord(1111, 2222, 3333, 4444, 5555.5555f, "GHI789");
+	Collection<RawOcrWord> rawTexts = Arrays.asList(rawOcrWord1, rawOcrWord2, rawOcrWord3);
+
+	List<CachedOcrText> expected = Arrays.asList(new CachedOcrText(1, rawOcrWord1, null), new CachedOcrText(2, rawOcrWord2, "I have changed"), new CachedOcrText(3, rawOcrWord3, null));
+
+	HsqlBackedWordCache testClass = new HsqlBackedWordCache();
+	testClass.storeRawOcrWords(rawTexts);
+
+	// when
+	testClass.modifyWord(2, "I have changed");
+
+	// then
+	String sql = "SELECT id, raw_ocr_word, corrected_text FROM ocr_word";
+	List<CachedOcrText> results = new ArrayList<>();
+	try (PreparedStatement pStat = con.prepareStatement(sql); ResultSet res = pStat.executeQuery();) {
+	    while (res.next()) {
+		int id = res.getInt(1);
+		RawOcrWord rawOcrWord = (RawOcrWord) res.getObject(2);
+		String correctedText = res.getString(3);
+		results.add(new CachedOcrText(id, rawOcrWord, correctedText));
+	    }
+	}
+
+	assertEquals(expected, results);
+    }
+
+    @Test
+    void testGetWords() {
+	// given
+	RawOcrWord rawOcrWord1 = new RawOcrWord(11, 22, 33, 44, 55.55f, "ABC123");
+	RawOcrWord rawOcrWord2 = new RawOcrWord(111, 222, 333, 444, 555.555f, "DEF456");
+	RawOcrWord rawOcrWord3 = new RawOcrWord(1111, 2222, 3333, 4444, 5555.5555f, "GHI789");
+	Collection<RawOcrWord> rawTexts = Arrays.asList(rawOcrWord1, rawOcrWord2, rawOcrWord3);
+
+	List<CachedOcrText> expected = Arrays.asList(new CachedOcrText(1, rawOcrWord1, null), new CachedOcrText(2, rawOcrWord2, null), new CachedOcrText(3, rawOcrWord3, null));
+
+	HsqlBackedWordCache testClass = new HsqlBackedWordCache();
+	testClass.storeRawOcrWords(rawTexts);
+
+	// when
+	Collection<CachedOcrText> results = testClass.getWords();
+
+	// then
+	assertEquals(expected, results);
+    }
+
+    @Test
+    void testGetWord() {
+	// given
+	RawOcrWord rawOcrWord1 = new RawOcrWord(11, 22, 33, 44, 55.55f, "ABC123");
+	RawOcrWord rawOcrWord2 = new RawOcrWord(111, 222, 333, 444, 555.555f, "DEF456");
+	RawOcrWord rawOcrWord3 = new RawOcrWord(1111, 2222, 3333, 4444, 5555.5555f, "GHI789");
+	Collection<RawOcrWord> rawTexts = Arrays.asList(rawOcrWord1, rawOcrWord2, rawOcrWord3);
+
+	HsqlBackedWordCache testClass = new HsqlBackedWordCache();
+	testClass.storeRawOcrWords(rawTexts);
+
+	// when
+	CachedOcrText result = testClass.getWord(3);
+
+	// then
+	assertEquals(new CachedOcrText(3, rawOcrWord3, null), result);
+    }
+
 }
