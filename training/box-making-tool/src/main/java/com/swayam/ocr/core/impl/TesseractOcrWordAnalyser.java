0fe7ff9cfa4f1349dfd0dee91b5cbfe146f42560
Palash Ray <paawak@gmail.com>
2020-04-27 00:56:27 +0530

    added back previous code for line analysis

--- training/box-making-tool/src/main/java/com/swayam/ocr/core/impl/TesseractOcrWordAnalyser.java
+++ training/box-making-tool/src/main/java/com/swayam/ocr/core/impl/TesseractOcrWordAnalyser.java
@@ -1,8 +1,11 @@
 package com.swayam.ocr.core.impl;
 
+import static org.bytedeco.leptonica.global.lept.L_CLONE;
+import static org.bytedeco.leptonica.global.lept.boxaGetBox;
 import static org.bytedeco.leptonica.global.lept.pixDestroy;
 import static org.bytedeco.leptonica.global.lept.pixRead;
 
+import java.nio.IntBuffer;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -12,7 +15,10 @@ import java.util.function.Supplier;
 
 import org.bytedeco.javacpp.BytePointer;
 import org.bytedeco.javacpp.IntPointer;
+import org.bytedeco.leptonica.BOX;
+import org.bytedeco.leptonica.BOXA;
 import org.bytedeco.leptonica.PIX;
+import org.bytedeco.leptonica.PIXA;
 import org.bytedeco.tesseract.ETEXT_DESC;
 import org.bytedeco.tesseract.ResultIterator;
 import org.bytedeco.tesseract.TessBaseAPI;
@@ -37,7 +43,46 @@ public class TesseractOcrWordAnalyser {
 
     public Collection<RawOcrWord> getDetectedWords() {
 	LOGGER.info("Image file to analyse with Tesseract OCR: {}", imagePath);
+	analyzeLinesInImage();
+	return Collections.unmodifiableList(extractWordsFromImage());
+    }
 
+    private void analyzeLinesInImage() {
+
+	LOGGER.info("Image file to analyse with Tesseract OCR: {}", imagePath);
+
+	try (TessBaseAPI api = new TessBaseAPI();) {
+	    int returnCode = api.Init(TESSDATA_DIRECTORY, LANGUAGE_CODE);
+	    if (returnCode != 0) {
+		throw new RuntimeException("could not initialize tesseract, error code: " + returnCode);
+	    }
+
+	    PIX image = pixRead(imagePath.toFile().getAbsolutePath());
+
+	    api.SetImage(image);
+
+	    BOXA boxes = api.GetComponentImages(tesseract.RIL_TEXTLINE, true, (PIXA) null, (IntBuffer) null);
+
+	    LOGGER.info("Found {} textline image components.", boxes.n());
+
+	    for (int i = 0; i < boxes.n(); i++) {
+		BOX box = boxaGetBox(boxes, i, L_CLONE);
+		api.SetRectangle(box.x(), box.y(), box.w(), box.h());
+		BytePointer ocrResult = api.GetUTF8Text();
+		String ocrText = ocrResult.getString().trim();
+		ocrResult.deallocate();
+		int conf = api.MeanTextConf();
+		LOGGER.info("Box[{}]: x={}, y={}, w={}, h={}, confidence: {}, text: {}", i, box.x(), box.y(), box.w(), box.h(), conf, ocrText);
+	    }
+
+	    api.End();
+	    api.close();
+	    pixDestroy(image);
+	}
+
+    }
+
+    private List<RawOcrWord> extractWordsFromImage() {
 	Supplier<IntPointer> intPointerSupplier = () -> new IntPointer(new int[1]);
 
 	List<RawOcrWord> words = new ArrayList<>();
@@ -57,45 +102,44 @@ public class TesseractOcrWordAnalyser {
 		throw new IllegalArgumentException("could not recognize text");
 	    }
 
-	    ResultIterator ri = api.GetIterator();
-	    int level = tesseract.RIL_WORD;
+	    try (ResultIterator ri = api.GetIterator();) {
+		int level = tesseract.RIL_WORD;
 
-	    do {
-		BytePointer ocrResult = ri.GetUTF8Text(level);
-		String ocrText = ocrResult.getString().trim();
+		do {
+		    BytePointer ocrResult = ri.GetUTF8Text(level);
+		    String ocrText = ocrResult.getString().trim();
 
-		float conf = ri.Confidence(level);
-		IntPointer x1 = intPointerSupplier.get();
-		IntPointer y1 = intPointerSupplier.get();
-		IntPointer x2 = intPointerSupplier.get();
-		IntPointer y2 = intPointerSupplier.get();
-		boolean foundRectangle = ri.BoundingBox(level, x1, y1, x2, y2);
+		    float conf = ri.Confidence(level);
+		    IntPointer x1 = intPointerSupplier.get();
+		    IntPointer y1 = intPointerSupplier.get();
+		    IntPointer x2 = intPointerSupplier.get();
+		    IntPointer y2 = intPointerSupplier.get();
+		    boolean foundRectangle = ri.BoundingBox(level, x1, y1, x2, y2);
 
-		if (!foundRectangle) {
-		    throw new IllegalArgumentException("Could not find any rectangle here");
-		}
+		    if (!foundRectangle) {
+			throw new IllegalArgumentException("Could not find any rectangle here");
+		    }
 
-		RawOcrWord textBox = new RawOcrWord(x1.get(), y1.get(), x2.get(), y2.get(), conf, ocrText);
-		LOGGER.info("{}", textBox);
+		    RawOcrWord textBox = new RawOcrWord(x1.get(), y1.get(), x2.get(), y2.get(), conf, ocrText);
+		    LOGGER.info("{}", textBox);
 
-		words.add(textBox);
+		    words.add(textBox);
 
-		x1.deallocate();
-		y1.deallocate();
-		x2.deallocate();
-		y2.deallocate();
-		ocrResult.deallocate();
-	    } while (ri.Next(level));
+		    x1.deallocate();
+		    y1.deallocate();
+		    x2.deallocate();
+		    y2.deallocate();
+		    ocrResult.deallocate();
+		} while (ri.Next(level));
 
-	    ri.close();
-	    ri.deallocate();
+		ri.deallocate();
+	    }
 	    api.End();
-	    api.close();
 	    api.deallocate();
 	    pixDestroy(image);
 	}
 
-	return Collections.unmodifiableList(words);
+	return words;
     }
 
 }
