c964570432b74f7b29975a671e5b676c75c62cd8
Palash Ray <paawak@gmail.com>
2020-05-15 00:18:51 +0530

    passing values to OcrWord

--- training/box-making-tool-web/src/App.jsx
+++ training/box-making-tool-web/src/App.jsx
@@ -6,13 +6,13 @@ class App extends React.Component {
     return (
       <div class="container">
         <div class="row row-cols-5">
-          <OcrWord/>
-          <OcrWord/>
-          <OcrWord/>
-          <OcrWord/>
-          <OcrWord/>
-          <OcrWord/>
-          <OcrWord/>
+          <OcrWord wordId="1" givenText="aaa"/>
+          <OcrWord wordId="2" givenText="bbb"/>
+          <OcrWord wordId="3" givenText="ccc"/>
+          <OcrWord wordId="4" givenText="ddd"/>
+          <OcrWord wordId="5" givenText="eee"/>
+          <OcrWord wordId="6" givenText="fff"/>
+          <OcrWord wordId="7" givenText="ggg"/>
         </div>
       </div>
     );
--- training/box-making-tool-web/src/OcrWord.jsx
+++ training/box-making-tool-web/src/OcrWord.jsx
@@ -2,24 +2,26 @@ import React from 'react';
 
 class OcrWord extends React.Component {
   render() {
+    const givenTextInputId = `givenText_${this.props.wordId}`;
+    const correctedTextInputId = `correctedText_${this.props.wordId}`;
     return (
       <div class="col">
         <div class="container" id="word_1">
           <div class="row row-cols-1">
             <div class="col">
-              <img id="word-1" src="mockups/images/1.png"/>
+              <img id="word-1" src={"mockups/images/" + this.props.wordId + ".png"}/>
             </div>
             <div class="col">
-              <label for="ocrWord1">OCR:</label>
+              <label for={givenTextInputId}>OCR:</label>
             </div>
             <div class="col">
-              <input type="text" name="ocrWord1" id="ocrWord1" value="aaa" disabled="true"/>
+              <input type="text" name={givenTextInputId} id={givenTextInputId} value={this.props.givenText} disabled="true"/>
             </div>
             <div class="col">
-              <label for="correction1">Correction: </label>
+              <label for={correctedTextInputId}>Correction: </label>
             </div>
             <div class="col">
-              <input type="text" name="correction1" id="correction1"/>
+              <input type="text" name={correctedTextInputId} id={correctedTextInputId}/>
             </div>
           </div>
         </div>
