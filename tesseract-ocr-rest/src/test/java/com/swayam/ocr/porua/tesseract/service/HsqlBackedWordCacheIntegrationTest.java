522831255bb3897c72e8640e8aab049288d40530
Palash Ray <paawak@gmail.com>
2020-05-16 18:30:00 +0530

    specify active profile for test

--- tesseract-ocr-rest/src/test/java/com/swayam/ocr/porua/tesseract/service/HsqlBackedWordCacheIntegrationTest.java
+++ tesseract-ocr-rest/src/test/java/com/swayam/ocr/porua/tesseract/service/HsqlBackedWordCacheIntegrationTest.java
@@ -15,13 +15,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 import com.swayam.ocr.porua.tesseract.model.CachedOcrText;
 import com.swayam.ocr.porua.tesseract.model.Language;
 import com.swayam.ocr.porua.tesseract.model.RawOcrWord;
 
-@TestPropertySource(locations = { "classpath:/application-test.yml" })
+@ActiveProfiles("test")
+@TestPropertySource("classpath:/application-test.yml")
 @SpringBootTest
 class HsqlBackedWordCacheIntegrationTest {
 
