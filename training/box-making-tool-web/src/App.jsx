748cec92b015cbcbf3c50b166a405b2e63a871df
Palash Ray <paawak@gmail.com>
2020-05-17 23:06:14 +0530

    separate list of ocr words in a component

--- training/box-making-tool-web/src/App.jsx
+++ training/box-making-tool-web/src/App.jsx
@@ -1,34 +1,16 @@
 import React from 'react';
-import OcrWord from './OcrWord'
+import OcrCorrectionPage from './OcrCorrectionPage'
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      hasErrors: false,
-      ocrWords: []
-    };
-  }
-
-  componentDidMount() {
-    fetch("http://localhost:8080/train/word?imagePath=/kaaj/source/porua/training/box-making-tool/src/test/resources/images/bangla-mahabharat-1-page_2.jpg&language=ben")
-      .then(rawData => rawData.json())
-      .then(ocrWords => this.setState({ ocrWords: ocrWords }))
-      .catch(() => this.setState({ hasErrors: true }));
   }
 
   render() {
-    const ocrWords = this.state.ocrWords.map((ocrWord) =>
-      <OcrWord key={ocrWord.wordSequenceNumber} wordId={ocrWord.wordSequenceNumber} givenText={ocrWord.text}/>
-    );
 
     return (
-      <div className="container">
-        <div className="row row-cols-5">
-          {ocrWords}
-        </div>
-      </div>
+      <OcrCorrectionPage/>
     );
   }
 
---
+++ training/box-making-tool-web/src/OcrCorrectionPage.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import OcrWord from './OcrWord'
+
+class OcrCorrectionPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasErrors: false,
+      ocrWords: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8080/train/word?imagePath=/kaaj/source/porua/training/box-making-tool/src/test/resources/images/bangla-mahabharat-1-page_2.jpg&language=ben")
+      .then(rawData => rawData.json())
+      .then(ocrWords => this.setState({ ocrWords: ocrWords }))
+      .catch(() => this.setState({ hasErrors: true }));
+  }
+
+  render() {
+    const ocrWords = this.state.ocrWords.map((ocrWord) =>
+      <OcrWord key={ocrWord.wordSequenceNumber} wordId={ocrWord.wordSequenceNumber} givenText={ocrWord.text}/>
+    );
+
+    return (
+      <div className="container">
+        <div className="row row-cols-5">
+          {ocrWords}
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default OcrCorrectionPage;
