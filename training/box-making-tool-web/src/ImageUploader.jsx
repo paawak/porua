11552aa14d2e9f35d6c82fde806ac41aeb4a346b
Palash Ray <paawak@gmail.com>
2020-05-17 23:45:18 +0530

    further layouting

--- training/box-making-tool-web/src/ImageUploader.jsx
+++ training/box-making-tool-web/src/ImageUploader.jsx
@@ -9,30 +9,38 @@ class ImageUploader extends React.Component {
 
   render() {
     return (
-      <form>
-              <div className="form-group form-check">
-                <input className="form-check-input" type="radio" name="language" id="languageBengali" value="ben" checked={true}/>
-                <label className="form-check-label" htmlFor="languageBengali">
-                  Bengali
-                </label>
-              </div>
-              <div className="form-group form-check">
-                <input className="form-check-input" type="radio" name="language" id="languageEnglish" value="eng" />
-                <label className="form-check-label" htmlFor="languageEnglish">
-                  English
-                </label>
-              </div>
-      <div class="input-group mb-3">
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
-        </div>
-        <div class="custom-file">
-          <input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01"/>
-          <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
-        </div>
-      </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
+      	<form>
+      		<fieldset class="form-group">
+      			<div class="row">
+      				<legend class="col-form-label col-sm-2 pt-0">Language Selection</legend>
+      				<div class="col-sm-10">
+      					<div class="form-group form-check">
+      						<input className="form-check-input" type="radio" name="language"
+      							id="languageBengali" value="ben" checked={true} /> <label
+      							className="form-check-label" htmlFor="languageBengali">
+      							Bengali </label>
+      					</div>
+      					<div class="form-group form-check">
+      						<input className="form-check-input" type="radio" name="language"
+      							id="languageEnglish" value="eng" /> <label
+      							className="form-check-label" htmlFor="languageEnglish">
+      							English </label>
+      					</div>
+      				</div>
+      			</div>
+      		</fieldset>
+      		<div class="input-group mb-3">
+      			<div class="input-group-prepend">
+      				<span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
+      			</div>
+      			<div class="custom-file">
+      				<input type="file" class="custom-file-input" id="inputGroupFile01"
+      					aria-describedby="inputGroupFileAddon01" /> <label
+      					class="custom-file-label" for="inputGroupFile01">Choose file</label>
+      			</div>
+      		</div>
+      		<button type="submit" class="btn btn-primary">Submit</button>
+      	</form>
     );
   }
 
