141c1605ed5f17b78b0eda1f2055af4822e9fb73
Palash Ray <paawak@gmail.com>
2020-05-25 01:27:30 +0530

    migrate to new model

--- tesseract-ocr-rest/src/main/java/com/swayam/ocr/porua/tesseract/rest/OCRTrainingController.java
+++ tesseract-ocr-rest/src/main/java/com/swayam/ocr/porua/tesseract/rest/OCRTrainingController.java
@@ -1,11 +1,20 @@
 package com.swayam.ocr.porua.tesseract.rest;
 
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Optional;
+
+import javax.imageio.ImageIO;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.multipart.FilePart;
@@ -16,11 +25,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.swayam.ocr.porua.tesseract.dto.OcrWordDto;
+import com.swayam.ocr.porua.tesseract.OcrWordId;
+import com.swayam.ocr.porua.tesseract.model.Language;
+import com.swayam.ocr.porua.tesseract.model.OcrWord;
+import com.swayam.ocr.porua.tesseract.model.RawImage;
 import com.swayam.ocr.porua.tesseract.service.FileSystemUtil;
+import com.swayam.ocr.porua.tesseract.service.TesseractOcrWordAnalyser;
 import com.swayam.ocr.porua.tesseract.service.WordCache;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -39,72 +53,48 @@ public class OCRTrainingController {
     }
 
     @PostMapping(value = "/word", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Flux<OcrWordDto> getDetectedText(@RequestPart("language") String languageAsString, @RequestPart("image") FilePart image) throws IOException, URISyntaxException {
+    public Flux<OcrWord> getDetectedText(@RequestPart("language") final String languageAsString, @RequestPart("bookId") final long bookId,
+	    @RequestPart(value = "rawImageId", required = false) final Optional<Long> rawImageId, @RequestPart("pageNumber") final int pageNumber, @RequestPart("image") final FilePart image)
+	    throws IOException, URISyntaxException {
 
-	// LOG.info("FileName: {}, language: {}", image.filename(),
-	// languageAsString);
-	//
-	// Language language = Language.valueOf(languageAsString);
-	//
-	// String imageFileName = image.filename();
-	// Path savedImagePath = fileSystemUtil.saveMultipartFileAsImage(image);
-	//
-	// // TODO: hard-coded id
-	// if (wordCache.getWordCount(1, imageFileName) == 0) {
-	// RawImage rawImage = new RawImage();
-	// rawImage.setName(imageFileName);
-	// // TODO: hard-coded page
-	// rawImage.setPageNumber(1);
-	// long imageFileId = wordCache.storeImageFile(rawImage).getId();
-	// return Flux.create((FluxSink<RawOcrWord> fluxSink) -> {
-	// new TesseractOcrWordAnalyser(savedImagePath,
-	// language).extractWordsFromImage(fluxSink);
-	// }).doOnNext(rawText -> wordCache.storeRawOcrWord(imageFileId,
-	// rawText));
-	// }
-	//
-	// LOG.warn("Entries already present for {}: using existing entries",
-	// imageFileName);
-	//
-	// Function<CachedOcrText, RawOcrWord> transformWithCorrectedText =
-	// (cachedOcrText) -> {
-	// RawOcrWord originalWord = cachedOcrText.getRawOcrText();
-	// if (StringUtils.hasText(cachedOcrText.getCorrectText())) {
-	// return new RawOcrWord(originalWord.getX1(), originalWord.getY1(),
-	// originalWord.getX2(), originalWord.getY2(),
-	// originalWord.getConfidence(), cachedOcrText.getCorrectText(),
-	// originalWord.getWordSequenceNumber());
-	// }
-	//
-	// return originalWord;
-	// };
+	LOG.info("FileName: {}, language: {}", image.filename(), languageAsString);
 
-	// return Flux.fromIterable(wordCache.getWords(1,
-	// imageFileName)).map(transformWithCorrectedText);
+	Language language = Language.valueOf(languageAsString);
 
-	return null;
+	String imageFileName = image.filename();
+	Path savedImagePath = fileSystemUtil.saveMultipartFileAsImage(image);
+
+	if (rawImageId.isEmpty()) {
+	    RawImage rawImage = new RawImage();
+	    rawImage.setName(imageFileName);
+	    rawImage.setPageNumber(pageNumber);
+	    long imageFileId = wordCache.storeImageFile(rawImage).getId();
+	    return Flux.create((FluxSink<OcrWord> fluxSink) -> {
+		new TesseractOcrWordAnalyser(savedImagePath, language).extractWordsFromImage(fluxSink, (wordSequenceId) -> new OcrWordId(bookId, imageFileId, wordSequenceId));
+	    }).map(rawText -> wordCache.addOcrWord(rawText));
+	}
+
+	LOG.warn("Entries already present for {}: using existing entries", imageFileName);
+
+	return Flux.fromIterable(wordCache.getWords(bookId, rawImageId.get()));
 
     }
 
     @GetMapping(value = "/word/image")
-    public Mono<ResponseEntity<byte[]>> getOcrWordImage(@RequestParam("wordId") int wordId, @RequestParam("imagePath") Path imagePath) throws IOException {
-	// LOG.trace("serving ocr word image for id: {}", wordId);
-	// CachedOcrText ocrText = wordCache.getWord(wordId);
-	// BufferedImage fullImage =
-	// ImageIO.read(Files.newInputStream(imagePath));
-	// Rectangle wordArea =
-	// TesseractOcrWordAnalyser.getWordArea(ocrText.getRawOcrText());
-	// BufferedImage wordImage = fullImage.getSubimage(wordArea.x,
-	// wordArea.y, wordArea.width, wordArea.height);
-	// String extension = imagePath.toFile().getName().split("\\.")[1];
-	// ByteArrayOutputStream bos = new ByteArrayOutputStream();
-	// ImageIO.write(wordImage, extension, bos);
-	// bos.flush();
-	// HttpHeaders responseHeaders = new HttpHeaders();
-	// responseHeaders.set("content-type", "image/" + extension);
-	// return Mono.just(new ResponseEntity<byte[]>(bos.toByteArray(),
-	// responseHeaders, HttpStatus.OK));
-	return null;
+    public Mono<ResponseEntity<byte[]>> getOcrWordImage(@RequestParam("bookId") final long bookId, @RequestParam("rawImageId") final long rawImageId,
+	    @RequestParam("wordSequenceId") int wordSequenceId, @RequestParam("imagePath") Path imagePath) throws IOException {
+	LOG.trace("serving ocr word image for id: {}", wordSequenceId);
+	OcrWord ocrText = wordCache.getWord(new OcrWordId(bookId, rawImageId, wordSequenceId));
+	BufferedImage fullImage = ImageIO.read(Files.newInputStream(imagePath));
+	Rectangle wordArea = TesseractOcrWordAnalyser.getWordArea(ocrText);
+	BufferedImage wordImage = fullImage.getSubimage(wordArea.x, wordArea.y, wordArea.width, wordArea.height);
+	String extension = imagePath.toFile().getName().split("\\.")[1];
+	ByteArrayOutputStream bos = new ByteArrayOutputStream();
+	ImageIO.write(wordImage, extension, bos);
+	bos.flush();
+	HttpHeaders responseHeaders = new HttpHeaders();
+	responseHeaders.set("content-type", "image/" + extension);
+	return Mono.just(new ResponseEntity<byte[]>(bos.toByteArray(), responseHeaders, HttpStatus.OK));
     }
 
 }
