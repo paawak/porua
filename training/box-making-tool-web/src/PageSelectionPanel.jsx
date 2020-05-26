d9c3098d06794390e62aa3c7dd51a6d7c48cfeb9
Palash Ray <paawak@gmail.com>
2020-05-27 00:34:19 +0530

    making service calls to populate books, pages

--- training/box-making-tool-web/src/PageSelectionPanel.jsx
+++ training/box-making-tool-web/src/PageSelectionPanel.jsx
@@ -1,38 +1,75 @@
 import React from 'react';
 
+export const NEW_PAGE_OPTION = "NEW_PAGE_OPTION";
+
 class PageSelectionPanel extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      books: [],
+      pages: [],
+      selectedBookId: null
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8080/train/book")
+      .then(rawData => rawData.json())
+      .then(books => this.setState({ books: books }))
+      .catch(() => this.setState({ hasErrors: true }));
   }
 
   render() {
+
+    const bookItems = this.state.books.map((book) =>
+      <option key={book.id} value={book.id}>{book.name}</option>
+    );
+
+    const pageItems = this.state.pages.map((page) =>
+      <option key={page.id} value={page.id}>{page.name}</option>
+    );
 
     return (
       <form className="was-validated">
 
         <div className="mb-3">
           <label htmlFor="book">Book</label>
-          <select id="book" className="custom-select" required>
+          <select id="book" className="custom-select" required
+          onChange={e => {
+              let bookId = e.target.value;
+              this.setState({
+                pages: [],
+                selectedBookId: bookId
+              });
+
+              if (bookId !== '') {
+                fetch("http://localhost:8080/train/page?bookId=" + bookId)
+                  .then(rawData => rawData.json())
+                  .then(pages => this.setState({ pages: pages }))
+                  .catch(() => this.setState({ hasErrors: true }));
+              }
+
+            }
+          }
+          >
             <option value="">Choose...</option>
-            <option value="1">Book 1</option>
-            <option value="2">Book 2</option>
-            <option value="3">Book 3</option>
+            {bookItems}
           </select>
-          <div className="invalid-feedback">Select a Book</div>
+          <div className="invalid-feedback">* Select a Book</div>
         </div>
 
         <div className="mb-3">
           <label htmlFor="page">Page</label>
           <select id="page" className="custom-select" required>
             <option value="">Choose...</option>
-            <option value="1">Page 1</option>
-            <option value="2">Page 2</option>
-            <option value="3">Page 3</option>
+            <option value={NEW_PAGE_OPTION}>Add New Page</option>
+            {pageItems}
           </select>
-          <div className="invalid-feedback">Select a Page</div>
+          <div className="invalid-feedback">* Select a Page</div>
         </div>
+
+        <button type="button" className="btn btn-success">Submit</button>
 
       </form>
     );
