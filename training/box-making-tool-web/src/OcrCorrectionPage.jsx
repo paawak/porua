6a8f27077a9c0d3a10ed44856017a880f08522e0
Palash Ray <paawak@gmail.com>
2020-05-28 00:12:03 +0530

    handle error gracefully

--- training/box-making-tool-web/src/OcrCorrectionPage.jsx
+++ training/box-making-tool-web/src/OcrCorrectionPage.jsx
@@ -4,6 +4,18 @@ import OcrWord from './OcrWord'
 class OcrCorrectionPage extends React.Component {
 
   render() {
+
+    if (this.props.ocrWords.status !== 200) {
+      return (
+        <div className="alert alert-danger alert-dismissible fade show" role="alert">
+          <strong>Oops!</strong> Could not upload image for analysis.
+          <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+      );
+    }
+
     const ocrWords = this.props.ocrWords.map((ocrWord) =>
       <OcrWord key={ocrWord.ocrWordId.wordSequenceId} bookId={ocrWord.ocrWordId.bookId}
         pageImageId={ocrWord.ocrWordId.pageImageId} wordSequenceId={ocrWord.ocrWordId.wordSequenceId}
