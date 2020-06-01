b1cbf55bea1e60d9a1ad3b3af806cb3f6bcef2d6
Palash Ray <paawak@gmail.com>
2020-06-02 00:44:16 +0530

    working bangla

--- training/box-making-tool-web/src/BanglaTextBox.jsx
+++ training/box-making-tool-web/src/BanglaTextBox.jsx
@@ -5,13 +5,12 @@ const $ = window.$;
 class BanglaTextBox extends Component {
 
   componentDidMount(){
-    this.$el = $(this.el);
     $('input[class="banglaText"]').bangla({enable: true});
   }
   
   render() {
     return (
-      <input type='text' className="banglaText"  ref={el => this.el = el}  /> 
+      <input type='text' className="banglaText" /> 
     )
   }
 
