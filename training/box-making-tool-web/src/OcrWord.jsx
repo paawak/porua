984d83e0245ea3d53ddf51efd83ee158e9a5bc9b
Palash Ray <paawak@gmail.com>
2020-06-02 22:39:12 +0530

    fixed overflow, show borders

--- training/box-making-tool-web/src/OcrWord.jsx
+++ training/box-making-tool-web/src/OcrWord.jsx
@@ -15,26 +15,28 @@ class OcrWord extends React.Component {
     const correctedTextInputId = idGenerator("correctedText");
 
     return (
-      <div className="col">
-        <div className="container">
-          <div className="row row-cols-1">
-            <div className="col">
-              <img id={idGenerator("ocrImage")} alt="..." src={"http://localhost:8080/train/word/image?bookId=" + this.props.bookId + "&pageImageId=" + this.props.pageImageId + "&wordSequenceId=" + this.props.wordSequenceId}/>
-            </div>
-            <div className="col">
-              <label htmlFor={givenTextInputId}>OCR:</label>
-            </div>
-            <div className="col">
-              <input type="text" name={givenTextInputId} id={givenTextInputId} value={this.props.givenText} disabled={true}/>
-            </div>
-            <div className="col">
-              <label htmlFor={correctedTextInputId}>Correction: </label>
-            </div>
-            <div className="col">
-              <BanglaTextBox name={correctedTextInputId} id={correctedTextInputId}/>
-            </div>
-          </div>
-        </div>
+      <div className="col">                  
+          <div className="container">                                    
+              <div className="row row-cols-1 overflow-auto">
+              <span className="border border-primary">              
+                <div className="col">
+                  <img id={idGenerator("ocrImage")} alt="..." src={"http://localhost:8080/train/word/image?bookId=" + this.props.bookId + "&pageImageId=" + this.props.pageImageId + "&wordSequenceId=" + this.props.wordSequenceId}/>
+                </div>
+                <div className="col">
+                  <label htmlFor={givenTextInputId}>OCR:</label>
+                </div>
+                <div className="col">
+                  <input type="text" name={givenTextInputId} id={givenTextInputId} value={this.props.givenText} disabled={true}/>
+                </div>
+                <div className="col">
+                  <label htmlFor={correctedTextInputId}>Correction: </label>
+                </div>
+                <div className="col">
+                  <BanglaTextBox name={correctedTextInputId} id={correctedTextInputId}/>
+                </div>
+                </span>
+              </div>            
+          </div>        
       </div>
     );
   }
