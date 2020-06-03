93bde181ac5abe8347f89721c60d1445ad38549b
Palash Ray <paawak@gmail.com>
2020-06-03 11:30:52 +0530

    put padding and placeholders

--- training/box-making-tool-web/src/OcrWord.jsx
+++ training/box-making-tool-web/src/OcrWord.jsx
@@ -43,26 +43,23 @@ class OcrWord extends React.Component {
 
     return (
       <div className="col">                  
-          <div className="container">                                               
+          <div className="container p-2">                                               
               <div className={"row row-cols-1 overflow-auto border " + borderColorClass}>              
                 <div className="col">
                   <button type="button" id={closeButtonId} className="close" aria-label="Close" onClick={this.handleCloseButton}>
                     <span aria-hidden="true">&times;</span>
                   </button> 
                 </div>                       
-                <div className="col">
+                <div className="col pb-3">
                   <img id={idGenerator("ocrImage")} alt="..." src={"http://localhost:8080/train/word/image?bookId=" + this.props.bookId + "&pageImageId=" + this.props.pageImageId + "&wordSequenceId=" + this.props.wordSequenceId}/>
                 </div>
-                <div className="col alert alert-dark jumbotron jumbotron-fluid" role="alert">
+                <div className="col p-3 alert alert-dark jumbotron jumbotron-fluid" role="alert">
                   <p className="lead">OCR Text</p>
                   <hr className="my-4"/>
                   <h1 className="display-4">{this.props.givenText}</h1>
                 </div>
-                <div className="col">
-                  <label htmlFor={correctedTextInputId}>Correction: </label>
-                </div>
-                <div className="col">
-                  <BanglaTextBox name={correctedTextInputId} id={correctedTextInputId}/>
+                <div className="col p-3 form-group">
+                  <BanglaTextBox name={correctedTextInputId} id={correctedTextInputId} placeholder="Correct Text"/>
                 </div>
               </div>            
           </div>        
