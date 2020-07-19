fdadfa19fbf230a332d292c360fa415b54f13ebc
Palash Ray <paawak@gmail.com>
2020-07-19 22:57:56 +0530

    sticky top bar

--- training/box-making-tool-web/src/OcrCorrectionPage.jsx
+++ training/box-making-tool-web/src/OcrCorrectionPage.jsx
@@ -16,41 +16,25 @@ class OcrCorrectionPage extends React.Component {
 
     return (
       <div className="container">
-        <h2>Ocr Correction Page</h2>                             
-        <div id="bookDetailsCarouselControls" className="carousel slide" data-ride="carousel">
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              {this.getNameValueAsDiv("Book", this.props.page.book.name)}
-            </div>
-            <div className="carousel-item">
-              {this.getNameValueAsDiv("Language", this.props.page.book.language)}
-            </div>
-            <div className="carousel-item">
-              {this.getNameValueAsDiv("Page", this.props.page.name)}
+        <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+          <div className="navbar-brand">Ocr Correction Page</div>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <div className="nav-item nav-link">Language: {this.props.page.book.language}</div>
+              <div className="nav-item nav-link">Book: {this.props.page.book.name}</div>              
+              <div className="nav-item nav-link active">Page: {this.props.page.name} <span className="sr-only">(current)</span></div>
             </div>
           </div>
-          <a className="carousel-control-prev" href="#bookDetailsCarouselControls" role="button" data-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="sr-only">Previous</span>
-          </a>
-          <a className="carousel-control-next" href="#bookDetailsCarouselControls" role="button" data-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="sr-only">Next</span>
-          </a>
-        </div>
+          <button className="btn btn-outline-success my-2 my-sm-0" type="button">Submit For Correction</button>
+        </nav>             
         <div className="row row-cols-4">
           {ocrWords}
         </div>
       </div>
     );
-  }
-
-  getNameValueAsDiv(name, value) {
-    return <div className="d-block w-100 jumbotron jumbotron-fluid">
-            <div className="container">
-              <h1 className="display-4">{name}: {value}</h1>
-            </div>
-          </div>  
   }
 
 }
