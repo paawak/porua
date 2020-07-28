c9998bc87951b18e7ba1004838ab76a68fa3079c
Palash Ray <paawak@gmail.com>
2020-07-29 01:08:05 +0530

    serving static content

--- tesseract-ocr-rest/src/main/java/com/swayam/ocr/porua/tesseract/config/WebFluxConfig.java
+++ tesseract-ocr-rest/src/main/java/com/swayam/ocr/porua/tesseract/config/WebFluxConfig.java
@@ -1,9 +1,19 @@
 package com.swayam.ocr.porua.tesseract.config;
 
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 @Configuration
 @EnableWebFlux
@@ -12,6 +22,11 @@ public class WebFluxConfig implements WebFluxConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
 	corsRegistry.addMapping("/**/**").allowedOrigins("http://localhost:3000").allowedMethods("*").maxAge(3600);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> htmlRouter(@Value("classpath:/static/index.html") Resource html) {
+	return route(GET("/"), request -> ok().contentType(MediaType.TEXT_HTML).bodyValue(html));
     }
 
 }
