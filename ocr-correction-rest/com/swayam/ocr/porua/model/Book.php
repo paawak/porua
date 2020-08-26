b2da33e1b618568c8f69646e3a950a627e656340
Palash Ray <paawak@gmail.com>
2020-08-26 23:21:45 +0530

    php model class

---
+++ ocr-correction-rest/com/swayam/ocr/porua/model/Book.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace com\swayam\ocr\porua\model;
+
+/**
+ * Description of Book
+ *
+ * @author paawak
+ */
+class Book {
+    //put your code here
+}
