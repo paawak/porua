b8ca99a53491edcef5fdb8163977414d0d34c524
Palash Ray <paawak@gmail.com>
2020-08-27 23:59:38 +0530

    orm seems to be working

--- ocr-correction-rest/index.php
+++ ocr-correction-rest/index.php
@@ -1,9 +1,26 @@
 <?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
 
-require __DIR__ . '/vendor/autoload.php';
+$paths = array("com/swayam/ocr/porua/model");
+$isDevMode = true;
+
+// the connection configuration
+$dbParams = array(
+    'driver'   => 'pdo_mysql',
+    'user'     => 'root',
+    'password' => 'root123',
+    'dbname'   => 'porua',
+);
+
+$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+$entityManager = EntityManager::create($dbParams, $config);
 
 $app = AppFactory::create();
 
