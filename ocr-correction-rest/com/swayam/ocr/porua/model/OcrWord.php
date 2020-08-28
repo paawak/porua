377bd283bf29b887d9bdc00e1c9da6828023e571
Palash Ray <paawak@gmail.com>
2020-08-28 10:42:42 +0530

    imports, getters setters

--- ocr-correction-rest/com/swayam/ocr/porua/model/OcrWord.php
+++ ocr-correction-rest/com/swayam/ocr/porua/model/OcrWord.php
@@ -2,6 +2,10 @@
 
 namespace com\swayam\ocr\porua\model;
 
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\Table;
+use Doctrine\ORM\Mapping\Column;
+
 /**
  * @Entity
  * @Table(name="ocr_word")
@@ -43,5 +47,101 @@ class OcrWord {
 
     /** @Column(type="boolean") */
     private $ignored;
+
+    public function getBookId() {
+        return $this->bookId;
+    }
+
+    public function getPageImageId() {
+        return $this->pageImageId;
+    }
+
+    public function getWordSequenceId() {
+        return $this->wordSequenceId;
+    }
+
+    public function getRawText() {
+        return $this->rawText;
+    }
+
+    public function getCorrectedText() {
+        return $this->correctedText;
+    }
+
+    public function getX1() {
+        return $this->x1;
+    }
+
+    public function getY1() {
+        return $this->y1;
+    }
+
+    public function getX2() {
+        return $this->x2;
+    }
+
+    public function getY2() {
+        return $this->y2;
+    }
+
+    public function getConfidence() {
+        return $this->confidence;
+    }
+
+    public function getLineNumber() {
+        return $this->lineNumber;
+    }
+
+    public function getIgnored() {
+        return $this->ignored;
+    }
+
+    public function setBookId($bookId): void {
+        $this->bookId = $bookId;
+    }
+
+    public function setPageImageId($pageImageId): void {
+        $this->pageImageId = $pageImageId;
+    }
+
+    public function setWordSequenceId($wordSequenceId): void {
+        $this->wordSequenceId = $wordSequenceId;
+    }
+
+    public function setRawText($rawText): void {
+        $this->rawText = $rawText;
+    }
+
+    public function setCorrectedText($correctedText): void {
+        $this->correctedText = $correctedText;
+    }
+
+    public function setX1($x1): void {
+        $this->x1 = $x1;
+    }
+
+    public function setY1($y1): void {
+        $this->y1 = $y1;
+    }
+
+    public function setX2($x2): void {
+        $this->x2 = $x2;
+    }
+
+    public function setY2($y2): void {
+        $this->y2 = $y2;
+    }
+
+    public function setConfidence($confidence): void {
+        $this->confidence = $confidence;
+    }
+
+    public function setLineNumber($lineNumber): void {
+        $this->lineNumber = $lineNumber;
+    }
+
+    public function setIgnored($ignored): void {
+        $this->ignored = $ignored;
+    }
 
 }
--- ocr-correction-rest/com/swayam/ocr/porua/model/PageImage.php
+++ ocr-correction-rest/com/swayam/ocr/porua/model/PageImage.php
@@ -2,6 +2,12 @@
 
 namespace com\swayam\ocr\porua\model;
 
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\Table;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Column;
+
 /**
  * @Entity
  * @Table(name="page_image")
@@ -32,5 +38,53 @@ class PageImage {
 
     /** @Column(name = "correction_completed", type="boolean") */
     private $correctionCompleted;
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getBook() {
+        return $this->book;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getPageNumber() {
+        return $this->pageNumber;
+    }
+
+    public function getIgnored() {
+        return $this->ignored;
+    }
+
+    public function getCorrectionCompleted() {
+        return $this->correctionCompleted;
+    }
+
+    public function setId($id): void {
+        $this->id = $id;
+    }
+
+    public function setBook($book): void {
+        $this->book = $book;
+    }
+
+    public function setName($name): void {
+        $this->name = $name;
+    }
+
+    public function setPageNumber($pageNumber): void {
+        $this->pageNumber = $pageNumber;
+    }
+
+    public function setIgnored($ignored): void {
+        $this->ignored = $ignored;
+    }
+
+    public function setCorrectionCompleted($correctionCompleted): void {
+        $this->correctionCompleted = $correctionCompleted;
+    }
 
 }
