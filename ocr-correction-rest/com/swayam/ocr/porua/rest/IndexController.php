3433033781a10447a61227a26968a058e33053ac
Palash Ray <paawak@gmail.com>
2020-09-05 19:37:11 +0530

    use psr-7

--- ocr-correction-rest/com/swayam/ocr/porua/rest/IndexController.php
+++ ocr-correction-rest/com/swayam/ocr/porua/rest/IndexController.php
@@ -4,13 +4,14 @@ namespace com\swayam\ocr\porua\rest;
 
 use Doctrine\ORM\EntityManager;
 use Psr\Log\LoggerInterface;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 require_once __DIR__ . '/../model/Book.php';
 
 class IndexController {
 
     private $entityManager;
-
     private $logger;
 
     public function __construct(EntityManager $entityManager, LoggerInterface $logger) {
@@ -18,12 +19,13 @@ class IndexController {
         $this->logger = $logger;
     }
 
-    public function get($request, $response) {
+    public function get(Request $request, Response $response) {
         $this->logger->info("in index, get");
         $book = $this->entityManager->find('com\swayam\ocr\porua\model\Book', 1);
         $bookName = $book->getName();
-        $response->getBody()->write("Book: $bookName");
-        return $response;
+        $payload = json_encode(['book' => $bookName], JSON_PRETTY_PRINT);
+        $response->getBody()->write($payload);
+        return $response->withHeader('Content-Type', 'application/json');
     }
 
 }
