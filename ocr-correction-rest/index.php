1f95d59d8828c383239f1faf44cd26abe6b0beea
Palash Ray <paawak@gmail.com>
2020-09-05 20:00:15 +0530

    use error handling

--- ocr-correction-rest/index.php
+++ ocr-correction-rest/index.php
@@ -3,11 +3,28 @@
 require __DIR__ . '/com/swayam/ocr/porua/rest/IndexController.php';
 
 use DI\Bridge\Slim\Bridge;
+use Slim\Handlers\ErrorHandler;
+use Slim\Factory\ServerRequestCreatorFactory;
+use Psr\Log\LoggerInterface;
+
 use com\swayam\ocr\porua\rest\IndexController;
 
 $container = require __DIR__ . '/com/swayam/ocr/porua/config/bootstrap.php';
 
 $app = Bridge::create($container);
+
+$logger = $container->get(LoggerInterface::class);
+
+$displayErrorDetails = true;
+$callableResolver = $app->getCallableResolver();
+$responseFactory = $app->getResponseFactory();
+$serverRequestCreator = ServerRequestCreatorFactory::create();
+$request = $serverRequestCreator->createServerRequestFromGlobals();
+$errorHandler = new ErrorHandler($callableResolver, $responseFactory, $logger);
+
+$app->addRoutingMiddleware();
+$errorMiddleware = $app->addErrorMiddleware($displayErrorDetails, false, false);
+$errorMiddleware->setDefaultErrorHandler($errorHandler);
 
 $app->get('/', [IndexController::class, 'get']);
 
