dce728e67aecc57ecd008ab191071b95256f05a8
Palash Ray <paawak@gmail.com>
2020-09-06 20:06:56 +0530

    image displayed dynamic --- wip

--- ocr-correction-rest/com/swayam/ocr/porua/rest/TrainingController.php
+++ ocr-correction-rest/com/swayam/ocr/porua/rest/TrainingController.php
@@ -6,6 +6,9 @@ use Doctrine\ORM\EntityManager;
 use Psr\Log\LoggerInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use GuzzleHttp\Psr7\LazyOpenStream;
+use GuzzleHttp\Stream\Stream;
+use GuzzleHttp\Psr7;
 use com\swayam\ocr\porua\model\Book;
 use com\swayam\ocr\porua\model\PageImage;
 use com\swayam\ocr\porua\model\OcrWord;
@@ -15,6 +18,8 @@ require_once __DIR__ . '/../model/PageImage.php';
 require_once __DIR__ . '/../model/OcrWord.php';
 
 class TrainingController {
+
+    const IMAGE_STORE = '/kaaj/source/porua/tesseract-ocr-rest/images/';
 
     private $entityManager;
     private $logger;
@@ -61,23 +66,51 @@ class TrainingController {
         $response->getBody()->write($payload);
         return $response->withHeader('Content-Type', 'application/json');
     }
-    
+
     public function getWordImage(Request $request, Response $response) {
         $queryParams = $request->getQueryParams();
         $bookId = $queryParams["bookId"];
         $pageImageId = $queryParams["pageImageId"];
         $wordSequenceId = $queryParams["wordSequenceId"];
-        $page = $this->entityManager->getRepository(PageImage::class)->findOneBy(array(
-            'id' => $pageImageId
-        ));
+        $pageName = $this->entityManager->getRepository(PageImage::class)->findOneBy(array(
+                    'id' => $pageImageId
+                ))->getName();
+
+        $nameTokens = explode('.', $pageName);
+        $imageExtension = $nameTokens[count($nameTokens) - 1];
+
+        $imageFullPath = self::IMAGE_STORE . $pageName;
+
+//        echo '***' . $imageFullPath;
+
+        $imageStream = fopen($imageFullPath, 'rb');
+
         $ocrWord = $this->entityManager->getRepository(OcrWord::class)->findOneBy(array(
             'ocrWordId.bookId' => $bookId,
             'ocrWordId.pageImageId' => $pageImageId,
             'ocrWordId.wordSequenceId' => $wordSequenceId
         ));
-        $payload = json_encode($ocrWord, JSON_PRETTY_PRINT);
-        $response->getBody()->write($payload);
-        return $response->withHeader('Content-Type', 'application/json');
+
+        $im = imagecreatefrompng($imageFullPath);
+
+        header("Content-Type: image/png");
+        $image = @imagecreate(500, 500)
+                or die("Cannot Initialize new GD image stream");
+
+        imagecolorallocate($image, 0xff, 0, 0xff);
+        $text_color = imagecolorallocate($image, 0, 0, 0xee);
+
+        imagestring($image, 5, 5, 5, "Hello GD!!", $text_color);
+        imagepng($image);
+//        $stream = Psr7\stream_for($image);
+        imagedestroy($image);
+
+        $image1 = file_get_contents($imageFullPath);
+        if ($image1 === false) {
+            die("error getting image");
+        }
+//        $response->withBody(Psr7\stream_for($image1));
+        return $response->withHeader('Content-Type', 'image/png');
     }
 
 }
